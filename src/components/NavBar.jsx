28ad8b27df239888a61e18ce19e840fde6572407
Ignacio <ignacioelopez04@gmail.com>
2025-06-05 12:17:57 -0300

    ft: navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,29 @@
-import { useAuth } from "./hooks";
+import { Button } from 'primereact/button';
+import { useAuth } from './hooks';
 
 export function NavBar() {
   const { logout } = useAuth();
 
   return (
-    <nav className="navbar flex justify-between items-center bg-gray-800 p-4 text-white">
-      <div className="logo text-lg font-bold">Fundación Causana</div>
-      <ul className="menu flex space-x-4">
-        <li><a href="/home">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-      <div className="auth-buttons flex space-x-4">
-        <button className="bg-blue-500 px-4 py-2 rounded cursor-pointer" onClick={()=>logout()}>Salir</button>
+    <nav className="navbar flex justify-between items-center bg-gray-100 p-4 text-gray-500">
+      <div className="flex items-center">
+        <img src="" alt="icono" className="mr-4"></img>
+        <div className="logo text-lg font-bold">Fundación Causana</div>
+      </div>
+      <div className="flex items-center w-1/4 justify-between">
+        <ul className="menu flex space-x-4">
+          <li className="px-2 py-1 rounded hover:bg-gray-200">
+            <a href="/home">Inicio</a>
+          </li>
+          <li className="px-2 py-1 rounded hover:bg-gray-200">
+            <a href="/about">Pacientes</a>
+          </li>
+        </ul>
+        <Button
+          icon="pi pi-sign-out"
+          className="px-4 py-2 rounded cursor-pointer hover:bg-gray-200"
+          onClick={() => logout()}
+        ></Button>
       </div>
     </nav>
   );
