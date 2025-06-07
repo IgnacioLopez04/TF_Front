f7a31f6efc94721b8a5743de19d10a2c8a81249e
Ignacio <ignacioelopez04@gmail.com>
2025-06-07 18:32:40 -0300

    chore: correcciones

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,10 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+
 import { useAuth } from "../components/hooks";
-import { NavBar } from "../components/NavBar";
 
 export function Home() {
-  const { user, authenticated } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if(!authenticated) {
-      navigate('/login');
-    }
-  })
-
+  const { user } = useAuth();
   return (
     <>
-      <NavBar></NavBar>
       <div className="home">
         <h1>Home</h1>
         <p>Welcome to the home page!</p>
